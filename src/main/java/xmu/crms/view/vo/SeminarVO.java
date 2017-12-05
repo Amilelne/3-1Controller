c03922bbbc222f9ee537cb3f36b7beb9d2f9bba5
xingb <764856859@qq.com>
2017-12-05 16:20:09 +0800

    update seminar

--- src/main/java/xmu/crms/view/vo/SeminarVO.java
+++ src/main/java/xmu/crms/view/vo/SeminarVO.java
@@ -1,15 +1,38 @@
 package xmu.crms.view.vo;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+*
+* @author xingb
+* @date 2017/12/5
+*/
 public class SeminarVO {
 	private int id;
 	private String name;
 	private String description;
 	private String groupingMethod;
-	private Date startTime;
-	private Date endTime;
-	public SeminarVO(int id, String name, String description, String groupingMethod, Date startTime, Date endTime) {
+	private String startTime;
+	private String endTime;
+	private List<TopicVO> topics=new ArrayList<TopicVO>();
+	/**微信小程序不需要proportions**/
+	
+	/**my**/
+	private String courseName;
+	private int classCalling;
+	private boolean isLeader;
+	private boolean areTopicsSelected;
+	
+	/**detail**/
+	private String site;
+	private String teacherName;
+	private String teacherEmail;
+	
+	public SeminarVO() {
+		super();
+	}
+	public SeminarVO(int id, String name, String description, String groupingMethod, String startTime, String endTime) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -42,17 +65,68 @@ public class SeminarVO {
 	public void setGroupingMethod(String groupingMethod) {
 		this.groupingMethod = groupingMethod;
 	}
-	public Date getStartTime() {
+	public String getStartTime() {
 		return startTime;
 	}
-	public void setStartTime(Date startTime) {
+	public void setStartTime(String startTime) {
 		this.startTime = startTime;
 	}
-	public Date getEndTime() {
+	public String getEndTime() {
 		return endTime;
 	}
-	public void setEndTime(Date endTime) {
+	public void setEndTime(String endTime) {
 		this.endTime = endTime;
+	}
+	public List<TopicVO> getTopics() {
+		return topics;
+	}
+	public void setTopics(List<TopicVO> topics) {
+		this.topics = topics;
+	}
+	public void addTopic(TopicVO topic) {
+		topics.add(topic);
+	}
+	public int getClassCalling() {
+		return classCalling;
+	}
+	public void setClassCalling(int classCalling) {
+		this.classCalling = classCalling;
+	}
+	public boolean isLeader() {
+		return isLeader;
+	}
+	public void setLeader(boolean isLeader) {
+		this.isLeader = isLeader;
+	}
+	public boolean isAreTopicsSelected() {
+		return areTopicsSelected;
+	}
+	public void setAreTopicsSelected(boolean areTopicsSelected) {
+		this.areTopicsSelected = areTopicsSelected;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+	public String getTeacherName() {
+		return teacherName;
+	}
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+	public String getTeacherEmail() {
+		return teacherEmail;
+	}
+	public void setTeacherEmail(String teacherEmail) {
+		this.teacherEmail = teacherEmail;
 	}
 	
 }
