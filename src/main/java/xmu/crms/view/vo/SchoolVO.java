38778f0ee3632a021d4a60655dd5955ba12dba48
chengjin <24320152202727@xmu.stu.edu.cn>
2017-12-05 14:03:01 +0800

    me&school&topicvo

--- src/main/java/xmu/crms/view/vo/SchoolVO.java
+++ src/main/java/xmu/crms/view/vo/SchoolVO.java
@@ -1,5 +1,43 @@
 package xmu.crms.view.vo;
-
+/**
+ * @author:JennieCheng
+ */
 public class SchoolVO {
 
+	private int id;
+	private String name;
+	private String province;
+	private String city;
+	public SchoolVO(int id, String name, String province, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.province = province;
+		this.city = city;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 }
