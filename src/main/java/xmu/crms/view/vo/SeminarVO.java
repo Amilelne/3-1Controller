b54ffc2ecff6bd984bf87dbec91831f657a6b3d5
xingb <764856859@qq.com>
2017-12-05 13:37:22 +0800

    add data

--- src/main/java/xmu/crms/view/vo/SeminarVO.java
+++ src/main/java/xmu/crms/view/vo/SeminarVO.java
@@ -1,5 +1,58 @@
 package xmu.crms.view.vo;
 
-public class SeminarVO {
+import java.util.Date;
 
+public class SeminarVO {
+	private int id;
+	private String name;
+	private String description;
+	private String groupingMethod;
+	private Date startTime;
+	private Date endTime;
+	public SeminarVO(int id, String name, String description, String groupingMethod, Date startTime, Date endTime) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.groupingMethod = groupingMethod;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getGroupingMethod() {
+		return groupingMethod;
+	}
+	public void setGroupingMethod(String groupingMethod) {
+		this.groupingMethod = groupingMethod;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	
 }
